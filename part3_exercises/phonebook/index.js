5fd66abf12a3d8627e352ce2b678ed2c0547b57a
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-16 14:56:21 +0300

    Initial Commit to backend repository

---
+++ part3_exercises/phonebook/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const PORT = 3001
+app.listen(PORT, () => {
+   console.log(`Server is running at http://localhost:${PORT}`) 
+})
