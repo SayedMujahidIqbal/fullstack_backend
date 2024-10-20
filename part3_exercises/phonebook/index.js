4157997fc7b26b16dd0f137b1e92a735ccc99899
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-20 15:16:52 +0300

    part 3 exercise 3.13 to 3.14 phonebook database connectivety commit

--- part3_exercises/phonebook/index.js
+++ part3_exercises/phonebook/index.js
@@ -1,103 +1,56 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/person.js')
 
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-/////// Morgan middlewares logs request method, url , status and response length and response time
-app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
-app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms'
-  ].join(' ')
-}))
 
-////////////// Creating Token to return request body //////////////////
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
-
-app.use(morgan(':method :url :body'))
-
-const generateId = () => {
-  const maxId = persons.length > 0
-  ? Math.max(...persons.map(n => Number(n.id)))
-  : 0
-  return String(maxId + 1)
-}
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+      res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const personId = req.params.id
-  const person = persons.find(person => person.id === personId)
-  if(person){
+  Person.find(req.params.id).then(person => {
     res.json(person)
-  }else{
-    res.status(404).end()
-}
-})
-
-app.delete('/api/persons/:id', (req, res) => {
-  const personId = req.params.id
-  persons = persons.filter(person => person.id !== personId)
-  res.status(204).end()
+  })
 })
 
 app.post('/api/persons', (req, res) => {
-  const body = req.body
+    const body = req.body
 
-  if(!body.name || !body.number){
-    return res.status(400).json({
-      error: 'content missing'
-    })
-  } 
-  const alreadyExist = persons.find(person => person.name === body.name)
-  if(alreadyExist){
-    return res.status(400).json({
-      error: 'name must be unique'
-    })
-  } else {
-    const person = {
+    if(!body.name || !body.number){
+      return res.status(400).json({
+        error: 'content missing'
+      })
+    } 
+    const person = new Person({
       name: body.name,
-      number: body.number,
-      id: generateId()
-    }
-  
-    persons = persons.concat(person)
-    res.status(200).json(person)
-  }
+      number: body.number
+    })
+    person.save().then(savedPerson => {
+      res.json(savedPerson)
+    })
 })
 
 app.get('/info', (req, res) => {
@@ -114,7 +67,7 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${numberOfInfosInPhonebook} people</p><p>${new Date().toLocaleDateString('en-US', options)} ${new Date().toString().match(/([A-Z]+[\+-][0-9]+.*)/)[1]}</p>`)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
    console.log(`Server is running at http://localhost:${PORT}`) 
 })
