d63d82290f3335211de53810b5ef9dfb5fcb57f1
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-21 12:33:04 +0300

    Part 3 exercise 3.19 phonebook step 7 commit

--- part3_exercises/phonebook/index.js
+++ part3_exercises/phonebook/index.js
@@ -67,12 +67,6 @@ app.get('/api/persons/:id', (req, res, next) => {
 
 app.post('/api/persons', (req, res, next) => {
     const body = req.body
-
-    if(!body.name || !body.number){
-      return res.status(400).json({
-        error: 'content missing'
-      })
-    } 
     const person = new Person({
       name: body.name,
       number: body.number
@@ -89,14 +83,9 @@ app.delete('/api/persons/:id', (req, res, next) => {
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
+  const {name, number} = req.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(req.params.id, person, { new: true }).then(updatedPerson => {
+  Person.findByIdAndUpdate(req.params.id, {name, number}, { new: true, runValidators: true, context: 'query' }).then(updatedPerson => {
       res.json(updatedPerson)
   }).catch(error => next(error))
 })
@@ -125,7 +114,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if(error.nam === "ValidatorError"){
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
