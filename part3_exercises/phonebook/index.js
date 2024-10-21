f8c4014493ac210752bd12255bbc4c94cb6bceda
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-21 10:35:07 +0300

    Part 3 exercise 3.15 to 3.17 phonebook steps 3 to 5 commit

--- part3_exercises/phonebook/index.js
+++ part3_exercises/phonebook/index.js
@@ -45,19 +45,27 @@ morgan.token('body', req => {
 app.use(morgan(':method :url :body'))
 
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(persons => {
-      res.json(persons)
-    })
+      if(persons){
+        res.json(persons)
+      }else{
+        res.status(204).end()
+      }
+    }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.find(req.params.id).then(person => {
-    res.json(person)
-  })
+    if(person){
+      res.json(person)
+    }else{
+      res.status(204).end()
+    }
+  }).catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if(!body.name || !body.number){
@@ -71,7 +79,26 @@ app.post('/api/persons', (req, res) => {
     })
     person.save().then(savedPerson => {
       res.json(savedPerson)
-    })
+    }).catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id).then(result => {
+      res.status(204).end()
+  }).catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true }).then(updatedPerson => {
+      res.json(updatedPerson)
+  }).catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
@@ -87,6 +114,25 @@ app.get('/info', (req, res) => {
     }
     res.send(`<p>Phonebook has info for ${numberOfInfosInPhonebook} people</p><p>${new Date().toLocaleDateString('en-US', options)} ${new Date().toString().match(/([A-Z]+[\+-][0-9]+.*)/)[1]}</p>`)
 })
+
+const unKnownEndPoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unKnownEndPoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
