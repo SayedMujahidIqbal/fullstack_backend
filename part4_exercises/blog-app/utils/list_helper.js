892e9a4b5b66899d8d8614c926e52c9efc674ddc
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-22 15:25:41 +0300

    Part 4 exercise 4.4 blogapp likes calculate test commit

--- part4_exercises/blog-app/utils/list_helper.js
+++ part4_exercises/blog-app/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
     return 1
 }
 
+const calculateLikes = (blogs) => {
+
+
+    if(blogs.length === 0){
+        return 0
+    } else if(blogs.length === 1){
+        return blogs.likes
+    } else{
+        return blogs.reduce((totalLikes, curr) => {
+            return totalLikes + curr.likes
+        }, 0)
+    }
+}
+
+
 module.exports = {
-    dummy
+    dummy, calculateLikes
 }
