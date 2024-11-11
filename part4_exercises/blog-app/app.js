2e33016282a0254343e6999f41c8af475bbcbf63
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-11-11 10:36:18 +0300

    updates for testing

--- part4_exercises/blog-app/app.js
+++ part4_exercises/blog-app/app.js
@@ -30,6 +30,10 @@ app.use(middleware.userExtractor)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+if(process.env.NODE_ENV === 'test'){
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
 app.use(middleware.unknwonEndPoint)
 app.use(middleware.errorHandler)
 
