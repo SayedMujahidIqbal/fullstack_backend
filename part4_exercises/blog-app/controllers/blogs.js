edacd51b5842f0f24173aa7d319f1a167fca4e70
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-12-02 10:43:23 +0300

    comments route added

--- part4_exercises/blog-app/controllers/blogs.js
+++ part4_exercises/blog-app/controllers/blogs.js
@@ -1,70 +1,87 @@
-const jwt = require('jsonwebtoken')
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
-const mongoose = require('mongoose')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
-blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate('creator', {username: 1, name: 1})
-    response.json(blogs)
-})
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("creator", {
+    username: 1,
+    name: 1,
+  });
+  response.json(blogs);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-    const blog = await Blog.findById(request.params.id)
-    if(blog){
-        response.json(blog)
-    }else{
-        response.status(404).end()
-    }
-})
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog);
+  } else {
+    response.status(404).end();
+  }
+});
 
-blogsRouter.post('/', async (request, response) => {
-    const body = request.body
-    if(request.user){
-        const user = await User.findById(request.user._id)
-        const blog = new Blog({
-            title: body.title,
-            author: body.author,
-            url: body.url,
-            likes: body.likes,
-            creator: user.id
-        })
-        const savedBlog = await blog.save()
-        user.blogs = user.blogs.concat(savedBlog._id)
-        await user.save()
-        response.status(201).json(savedBlog)
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
+  if (request.user) {
+    const user = await User.findById(request.user._id);
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      creator: user.id,
+    });
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+    response.status(201).json(savedBlog);
+  } else {
+    response.status(401).send("invalid token");
+  }
+});
+
+blogsRouter.post("/:id/comments", async (request, response) => {
+  const { comment } = request.body;
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { $push: { comments: comment } },
+    { new: true, runValidators: true, context: "query" }
+  );
+  console.log(updatedBlog);
+  response.json(updatedBlog);
+});
+
+blogsRouter.delete("/:id", async (request, response) => {
+  const blogTobeDeleted = await Blog.findById(request.params.id);
+  if (request.user) {
+    const user = request.user;
+    if (user._id.toString() !== blogTobeDeleted.creator.toString()) {
+      response.status(400).json({ error: "Request not authorized" });
     } else {
-        response.status(401).send('invalid token')
+      await Blog.findByIdAndDelete(request.params.id);
+      const userWithUpdatedBlogs = await User.findById(user._id);
+      userWithUpdatedBlogs.blogs = userWithUpdatedBlogs.blogs.filter(
+        (blog) => blog._id.toString() !== request.params.id
+      );
+      await userWithUpdatedBlogs.save();
+      response.status(204).end();
     }
-})
+  } else {
+    response.status(401).send("invalid token");
+  }
+});
 
-blogsRouter.delete('/:id', async (request, response) => {
-    const blogTobeDeleted = await Blog.findById(request.params.id)
-    if(request.user){
-        const user = request.user
-        if(user._id.toString() !== blogTobeDeleted.creator.toString()){
-            response.status(400).json({ error: 'Request not authorized' }) 
-        } else {
-            await Blog.findByIdAndDelete(request.params.id)
-            const userWithUpdatedBlogs = await User.findById(user._id)
-            userWithUpdatedBlogs.blogs = userWithUpdatedBlogs.blogs.filter(blog => blog._id.toString() !== request.params.id)
-            await userWithUpdatedBlogs.save()
-            response.status(204).end()
-        }
-    } else {
-        response.status(401).send('invalid token')
-    }
-})
+blogsRouter.put("/:id", async (request, response) => {
+  const { title, author, url, likes } = request.body;
+  if (request.user) {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      { title, author, url, likes, creator: request.body.creator.id },
+      { new: true, runValidators: true, context: "query" }
+    );
+    response.json(updatedBlog);
+  } else {
+    response.status(401).json("invlalid token");
+  }
+});
 
-blogsRouter.put('/:id', async (request, response) => {
-    const { title, author, url, likes } = request.body
-	if(request.user){
-        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, { title, author, url, likes, creator: request.body.creator.id }, { new: true, runValidators: true, context: 'query'})
-        response.json(updatedBlog)
-	}else{
-        response.status(401).json('invlalid token')
-    }	
-})
-
-
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- part4_exercises/blog-app/models/blog.js
+++ part4_exercises/blog-app/models/blog.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    author: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    url: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    likes: {
-        type:Number,
-        default: 0
-    },
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-    }
-})
+  title: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  author: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  url: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  comments: [String],
+});
 
-blogSchema.set('toJSON', { 
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-module.exports = Blog
- 
+module.exports = Blog;
